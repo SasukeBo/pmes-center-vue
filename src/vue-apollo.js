0984a645516b5a9607499c55648a11d4c431cb25
汪波 <809754210@qq.com>
2020-04-29 16:16:48 +0800

    change host

--- src/vue-apollo.js
+++ src/vue-apollo.js
@@ -11,7 +11,7 @@ Vue.use(VueApollo)
 // Name of the localStorage item
 const AUTH_TOKEN = 'apollo-token'
 
-const APIHost = process.env.NODE_ENV === 'production' ? '192.168.13.156' : 'localhost'
+const APIHost = process.env.NODE_ENV === 'production' ? '192.168.5.146' : 'localhost'
 // Http endpoint
 const httpEndpoint = 'http://' + APIHost + '/api'
 // Files URL root
